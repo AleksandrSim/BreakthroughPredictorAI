67171d064d116455b829f3ae49fefc706b413d75
Aleksandr Simonyan <46563315+AleksandrSim@users.noreply.github.com>
2024-08-13 17:00:14 -0700

    feat : first commit

---
+++ utils/load_cfg.py
@@ -0,0 +1,12 @@
+import yaml
+
+
+def load_yaml(yaml_path):
+    with open(yaml_path, 'r') as f:
+        cfg = yaml.safe_load(f)
+        return cfg
+    
+
+if __name__ == '__main__':
+    path = 'cfg.yaml'
+    print(load_yaml(path))
